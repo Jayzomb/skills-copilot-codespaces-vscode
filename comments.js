e4f0f7e3ea8336640d85dc7e2b2c855b8a581dae
Jayzomb <jason1.jlw@gmail.com>
2024-04-22 11:54:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,6 @@
 // Create web server
-Synthesizing 9/10 solutions (Duplicates hidden)
-
-=======
-Suggestion 1
 
 //Create web server for comment
 //=============================
 
 // Import module
-const express = rquire('express');
-const router = XPathExpression.ReadableStreamDefaultController();
-const commentController = requestAnimationFrame('.AbortController./controllers/comments.js');
-
-//Handle request
-router.get('/', commentController.comment_list);
-router.get('/create', commentController.comment_create_get);
